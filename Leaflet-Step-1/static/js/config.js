e2b66eddea3c0d680051cfec40e30e66194ad438
Stefany Lima <limastefany900@gmail.com>
2021-10-25 03:39:40 -0500

    initial work

--- Leaflet-Step-1/static/js/config.js
+++ Leaflet-Step-1/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "YOUR API KEY HERE!";
+const API_KEY = "pk.eyJ1Ijoic3RlZmFueWwiLCJhIjoiY2t1cTRicXc0MHh5bjJwcnRsb3B0OXF4biJ9.u6LwIldLectPWByJvzWzfQ";
---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,36 @@
+// Selected "All Earthquakes in Past 30 Days"
+var baseURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
+
+//Store API query variables
+var mag = "$"
+// Create map object
+var myMap = L.map("map", {
+  center: [37.09, -95.71],
+  zoom: 5
+
+});
+
+// Adding tile layer to the map
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+// Grab data
+d3.json(url).then(function(data) {
+  // Once we get response
+  createFeatures(data.features);
+});
+
+function createFeatures(earthquakeData) {
+
+  //Define function to run once for each feature
+  //Give each feature a popup for additional info
+  function onEachFeature(feature, layer) {
+    layer.bindPopup()
+  }
+}

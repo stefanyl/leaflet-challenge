743f26169a0cc5cf6e47e37e83f77a35572e8176
Stefany Lima <limastefany900@gmail.com>
2021-11-20 21:36:09 -0600

    changed pink to #00f900

--- Leaflet-Step-1/static/js/logic2.js
+++ Leaflet-Step-1/static/js/logic2.js
@@ -28,7 +28,7 @@ function getColor(depth)
         case depth > 10:
             return "yellow";
         default:
-            return "pink"
+            return "#00f900"
     }
 }
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(data => {
